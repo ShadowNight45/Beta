64a8b639cdc7d50d344af2e588be4b0da547eb31
ShadowNight45 <153755157+ShadowNight45@users.noreply.github.com>
2024-03-06 17:59:46 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Fonction pour afficher la fenêtre de dialogue
+function afficherDialogue() {
+    alert("Nếu bạn đọc được thông báo nầy thì bạn đã bị lừa !");
+}
+
+// Appeler la fonction après 10 secondes
+setTimeout(afficherDialogue, 10000); // 10000 millisecondes = 10 secondes
